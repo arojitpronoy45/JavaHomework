40ed2209aca24cfc267124fc9889d21c4f91ce98
arojitpronoy45 <arojitpronoyqa@gmail.com>
2023-09-29 22:24:49 -0400

    hw9modfied

--- src/hw9Abstraction/MedicalSchool.java
+++ src/hw9Abstraction/MedicalSchool.java
@@ -1,6 +1,11 @@
 package hw9Abstraction;
 
 public abstract class MedicalSchool {
+	// yes we can create a construct inside an Abstract Class
+	public MedicalSchool() {
+
+	}
+
 	// non-abstract method
 	public void anatomyLab() {
 		System.out.println("This is an Anatomoy Lab method");
