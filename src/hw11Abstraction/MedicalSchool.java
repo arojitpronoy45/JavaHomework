f11fd25561be91ba7f9e7b5f9bdce97c7f71d2c5
arojitpronoy45 <arojitpronoyqa@gmail.com>
2023-09-29 22:52:16 -0400

    hw11

---
+++ src/hw11Abstraction/MedicalSchool.java
@@ -0,0 +1,16 @@
+package hw11Abstraction;
+
+public abstract class MedicalSchool {
+	// non-abstract method
+	public void anatomyLab() {
+		System.out.println("This is an Anatomoy Lab method");
+
+	}
+
+	public abstract void biochemistryLab(); // abstract method
+
+}
+
+// An abstract class can only inherit one abstract class or one regular class
+// An abstract class cannot inherit an interface by extends keyword
+// only one inheritance is possible in an abstract class
