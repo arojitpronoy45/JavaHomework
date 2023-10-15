f3afe20d80feb9d440ca50978fa8cf82cb5327d2
arojitprony45 <arojitpronoyqa@gmail.com>
2023-10-15 16:08:11 -0400

    hw11modified

--- src/hw11Abstraction/MedicalSchool.java
+++ src/hw11Abstraction/MedicalSchool.java
@@ -1,6 +1,13 @@
 package hw11Abstraction;
 
-public abstract class MedicalSchool {
+/*
+// An abstract class can inherit only one abstract class or one regular class by extends key word
+// An abstract class can't inherit an Interface by extends key word
+// implements keyword is used to inherit Interface in abstract class
+// an abstract class can inherit more than one Interface
+// an abstract class cannot inherit a regular class or abstract class by implements keyword
+ */
+public abstract class MedicalSchool extends NursingSchool {
 	// non-abstract method
 	public void anatomyLab() {
 		System.out.println("This is an Anatomoy Lab method");
--- src/hw2JavaBasics/MyCar.java
+++ src/hw2JavaBasics/MyCar.java
@@ -3,16 +3,11 @@ package hw2JavaBasics;
 public class MyCar {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Arojit Pronoy, and I love to drive," + "My car info is below: ");
+		System.out.println("My name is \"Arojit Pronoy\", and I love to drive," + " My car info is below: ");
 		System.out.println("\n\tCar color: Black");
 		System.out.println("\tCar model: SE");
 		System.out.println("\tCar Year: 2020");
 		System.out.println("\tWheel Drive: All wheel Drive");
-		//
-		
-		
-		
-		
 
 	}
 
