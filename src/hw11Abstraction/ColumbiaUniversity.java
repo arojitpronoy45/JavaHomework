73cc60b86799a93a4d9494586ec2cc8d7ad6aa63
arojitprony45 <arojitpronoyqa@gmail.com>
2023-10-15 16:18:47 -0400

    hw11update

--- src/hw11Abstraction/ColumbiaUniversity.java
+++ src/hw11Abstraction/ColumbiaUniversity.java
@@ -1,6 +1,12 @@
 package hw11Abstraction;
+/* A regular class can inherit only one (regular class or one abstract class) by extends keyword
+ * A regular class can't inherit an Interface by extends keyword
+ * implements keyword is used to inherit Interface in regular class
+ * a regular class can inherit more than one Interface
+ * a regular class cannot inherit a regular class or abstract class by implements keyword
+ */
 
-public class ColumbiaUniversity {
+public class ColumbiaUniversity extends MedicalSchool implements University, VocationalSchool {
 	public void chemistry() {
 		System.out.println("This is a Chemistry method");
 	}
@@ -12,6 +18,60 @@ public class ColumbiaUniversity {
 	// default constructor
 	public ColumbiaUniversity() {
 
+	}
+
+	@Override
+	public void vocationalInfo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void classSize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playGround() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void teacher() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void biochemistryLab() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void caring() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void commonRoom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void laboratory() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void languageClub() {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
--- src/hw11Abstraction/University.java
+++ src/hw11Abstraction/University.java
@@ -1,6 +1,11 @@
 package hw11Abstraction;
 
-public interface University {
+/*
+ * //An Interface can inherit more than one Interface (separated by comma) by extends keyword
+//An Interface can't inherit a regular class or an abstract class by extends keyword
+//implements keyword can't be used for inheritance inside Interface
+*/
+public interface University extends College {
 	public void classSize();
 
 	public void playGround();
@@ -17,9 +22,8 @@ public interface University {
 
 	// an interface cannot have constructor
 	/*
-	 * public University() { 
-	 * Yes, an interface can inherit other interfaces 
-	 * an interface cannot be inherited by a regular or abstract class by extends key
+	 * public University() { Yes, an interface can inherit other interfaces an
+	 * interface cannot be inherited by a regular or abstract class by extends key
 	 * word // More than one inheritance is possible
 	 * 
 	 * 
